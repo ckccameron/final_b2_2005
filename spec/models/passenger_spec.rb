92d6d43f2d5b47e392bd3fff9edb622bd3e3618c
Cam C <ckc.cameron@gmail.com>
2020-08-03 11:55:10 -0600

    Write methods tests for passenger count by age

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -5,4 +5,46 @@ RSpec.describe Passenger do
     it {should have_many :flight_passengers}
     it {should have_many(:flights).through(:flight_passengers)}
   end
+
+  describe "methods" do
+    it "#count_minors" do
+      southwest = Airline.create!(name: "Southwest")
+
+      flight = Flight.create!(number: "2345", date: "08/03/20", time: "11:00", departure_city: "Denver", arrival_city: "Los Angeles", airline: southwest)
+
+      passenger_1 = Passenger.create!(name: "Snoop", age: 48)
+      passenger_2 = Passenger.create!(name: "Game", age: 40)
+      passenger_3 = Passenger.create!(name: "Kendrick", age: 33)
+      passenger_4 = Passenger.create!(name: "Roscoe", age: 15)
+      passenger_5 = Passenger.create!(name: "Dwayne", age: 8)
+
+      FlightPassenger.create(flight: flight, passenger: passenger_1)
+      FlightPassenger.create(flight: flight, passenger: passenger_2)
+      FlightPassenger.create(flight: flight, passenger: passenger_3)
+      FlightPassenger.create(flight: flight, passenger: passenger_4)
+      FlightPassenger.create(flight: flight, passenger: passenger_5)
+
+      expect(Passenger.count_minors).to eq(2)
+    end
+
+    it "#count_adults" do
+      southwest = Airline.create!(name: "Southwest")
+
+      flight = Flight.create!(number: "2345", date: "08/03/20", time: "11:00", departure_city: "Denver", arrival_city: "Los Angeles", airline: southwest)
+
+      passenger_1 = Passenger.create!(name: "Snoop", age: 48)
+      passenger_2 = Passenger.create!(name: "Game", age: 40)
+      passenger_3 = Passenger.create!(name: "Kendrick", age: 33)
+      passenger_4 = Passenger.create!(name: "Roscoe", age: 15)
+      passenger_5 = Passenger.create!(name: "Dwayne", age: 8)
+
+      FlightPassenger.create(flight: flight, passenger: passenger_1)
+      FlightPassenger.create(flight: flight, passenger: passenger_2)
+      FlightPassenger.create(flight: flight, passenger: passenger_3)
+      FlightPassenger.create(flight: flight, passenger: passenger_4)
+      FlightPassenger.create(flight: flight, passenger: passenger_5)
+
+      expect(Passenger.count_adults).to eq(3)
+    end
+  end
 end
