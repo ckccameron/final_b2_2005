7cb8187271d7107bc8a3e6e29a77bee98904ad56
Cam C <ckc.cameron@gmail.com>
2020-08-03 12:03:17 -0600

    Write methods for count of minors and adults

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,4 +4,12 @@ class Flight < ApplicationRecord
   has_many :passengers, through: :flight_passengers
 
   validates_presence_of :number, :date, :time, :departure_city, :arrival_city
+
+  def count_minors
+    passengers.where(age: 0...18).count
+  end
+
+  def count_adults
+    passengers.where(age: 18..Float::INFINITY).count
+  end
 end
