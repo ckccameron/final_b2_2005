0047621625eef6a49d7e8d81b0597ebda0747c0c
Cam C <ckc.cameron@gmail.com>
2020-08-03 10:47:46 -0600

    Edit spec flight model vlaidations to fix routing issue

--- spec/features/flights/show_spec.rb
+++ spec/features/flights/show_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "flight show page" do
   it "displays flight's info, airline it belongs to and passengers on board" do
-    southwest = Airline.create(name: "Southwest")
+    southwest = Airline.create!(name: "Southwest")
 
-    flight_1 = Flight.create(number: "2345", date: "08/03/20", time: "11:00", departure_city: "Denver", arrival_city: "Los Angeles")
+    flight = Flight.create!(number: "2345", date: "08/03/20", time: "11:00", departure_city: "Denver", arrival_city: "Los Angeles", airline: southwest)
 
-    passenger_1 = Passenger.create(name: "Snoop", age: 48)
-    passenger_2 = Passenger.create(name: "Game", age: 40)
-    passenger_3 = Passenger.create(name: "Kendrick", age: 33)
+    passenger_1 = Passenger.create!(name: "Snoop", age: 48)
+    passenger_2 = Passenger.create!(name: "Game", age: 40)
+    passenger_3 = Passenger.create!(name: "Kendrick", age: 33)
 
-    FlightPassenger.create(flight: flight_1, passenger: passenger_1)
-    FlightPassenger.create(flight: flight_1, passenger: passenger_2)
-    FlightPassenger.create(flight: flight_1, passenger: passenger_3)
+    FlightPassenger.create(flight: flight, passenger: passenger_1)
+    FlightPassenger.create(flight: flight, passenger: passenger_2)
+    FlightPassenger.create(flight: flight, passenger: passenger_3)
 
-    visit "/flights/#{southwest.id}"
+    visit "/flights/#{flight.id}"
 
     expect(page).to have_content("Airline: Southwest")
     expect(page).to have_content("Number: 2345")
